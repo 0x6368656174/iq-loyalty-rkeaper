19e0b2fe75fdf8f6c8bd449fca08e827c7c637fd
Pavel Puchkov <0x6368656174@gmail.com>
2019-04-10 19:31:29 +1000

    try

--- lib.cpp
+++ lib.cpp
@@ -18,6 +18,9 @@
 
 #ifdef _WIN32
 #include <Windows.h>
+#define TO_XPALTSTR(x) std::to_wstring(x)
+#else
+#define TO_XPALTSTR(x) std::to_string(x)
 #endif
 
 #include "windows_types.h"
@@ -42,7 +45,7 @@ std::string to_utf8 (const utility::string_t& str)
 #endif
 }
 
-std::string to_cp1251 (const std::wstring& str)
+std::string to_cp1251 (const utility::string_t& str)
 {
 #ifdef _WIN32
   if( str.empty() ) return std::string();
@@ -55,10 +58,6 @@ std::string to_cp1251 (const std::wstring& str)
   return str;
 #endif
 }
-std::string to_cp1251 (const std::string& str)
-{
-  return str;
-}
 
 void Init()
 {
@@ -67,21 +66,21 @@ void Init()
     inited = true;
     boost::log::add_common_attributes();
     boost::log::add_file_log
-       (
-          boost::log::keywords::file_name = "iq-loyalty_%N.log",
-          boost::log::keywords::open_mode = std::ios_base::app,
-          boost::log::keywords::rotation_size = 5 * 1024 * 1024,
-          boost::log::keywords::auto_flush = true,
-          boost::log::keywords::format =
-             (
-                boost::log::expressions::stream
-                   << boost::log::expressions::format_date_time< boost::posix_time::ptime >("TimeStamp", "%b %d %Y %H:%M:%S") << " "
-                   << boost::log::expressions::attr<boost::log::attributes::current_process_id::value_type>("ProcessID") << ": "
-                   << "%local0-" << boost::log::trivial::severity
-                   << "-" << std::hex << std::setw(6) << std::setfill('0') << boost::log::expressions::attr< unsigned int >("LineID")
-                   << ": " << boost::log::expressions::smessage
-             )
-       );
+        (
+            boost::log::keywords::file_name = "iq-loyalty_%N.log",
+            boost::log::keywords::open_mode = std::ios_base::app,
+            boost::log::keywords::rotation_size = 5 * 1024 * 1024,
+            boost::log::keywords::auto_flush = true,
+            boost::log::keywords::format =
+                (
+                    boost::log::expressions::stream
+                        << boost::log::expressions::format_date_time< boost::posix_time::ptime >("TimeStamp", "%b %d %Y %H:%M:%S") << " "
+                        << boost::log::expressions::attr<boost::log::attributes::current_process_id::value_type>("ProcessID") << ": "
+                        << "%local0-" << boost::log::trivial::severity
+                        << "-" << std::hex << std::setw(6) << std::setfill('0') << boost::log::expressions::attr< unsigned int >("LineID")
+                        << ": " << boost::log::expressions::smessage
+                )
+        );
   }
 
   BOOST_LOG_TRIVIAL(info) << "============= INIT LIB =============";
@@ -93,27 +92,26 @@ void Done() {
 
 
 int GetCardInfoEx(
-   INT64 card_number,
-   DWORD restaurant_code,
-   DWORD unit_number,
-   card_info* card_info,
-   char* input_buffer,
-   DWORD input_length,
-   WORD input_kind,
-   char* output_buffer,
-   DWORD output_length,
-   WORD output_kind) {
-  if (card_number < 100000000) {
-    return 1;
-  }
-
-  if (card_number > 999999999) {
-    return 1;
-  }
+    INT64 card_number,
+    DWORD restaurant_code,
+    DWORD unit_number,
+    card_info* card_info,
+    char* input_buffer,
+    DWORD input_length,
+    WORD input_kind,
+    char* output_buffer,
+    DWORD output_length,
+    WORD output_kind) {
   try
   {
+    if (card_number < 100000000) {
+      return 1;
+    }
 
-    utility::string_t phone = _XPLATSTR("+79") + std::to_wstring(card_number);
+    if (card_number > 999999999) {
+      return 1;
+    }
+    utility::string_t phone = _XPLATSTR("+79") + TO_XPALTSTR(card_number);
 
     BOOST_LOG_TRIVIAL(info) << boost::format("Load loyalty cart for %1%") % to_utf8(phone);
 
@@ -147,13 +145,13 @@ int GetCardInfoEx(
                            "}");
 
     json::value request = json::value::object({
-                                                 {_XPLATSTR("query"), json::value::string(query)},
-                                                 {_XPLATSTR("operationName"), json::value::null()},
-                                                 {_XPLATSTR("variables"), json::value::object({
-                                                                                                 {_XPLATSTR("shopId"), json::value::string(_XPLATSTR("yagoda"))},
-                                                                                                 {_XPLATSTR("shopSecret"), json::value::string(_XPLATSTR("2"))},
-                                                                                                 {_XPLATSTR("clientPhone"), json::value::string(phone)},
-                                                                                              })},
+                                                  {_XPLATSTR("query"), json::value::string(query)},
+                                                  {_XPLATSTR("operationName"), json::value::null()},
+                                                  {_XPLATSTR("variables"), json::value::object({
+                                                                                                   {_XPLATSTR("shopId"), json::value::string(_XPLATSTR("yagoda"))},
+                                                                                                   {_XPLATSTR("shopSecret"), json::value::string(_XPLATSTR("2"))},
+                                                                                                   {_XPLATSTR("clientPhone"), json::value::string(phone)},
+                                                                                               })},
                                               });
 
     // Create http_client to send the request.
